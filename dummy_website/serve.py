4816f2585befea0b6161e6107daa7d96da5c282f
Shayan Goldstein <168611084+shayanG7@users.noreply.github.com>
2026-02-08 01:05:44 -0500

    made news group webpage better

--- dummy_website/serve.py
+++ dummy_website/serve.py
@@ -2,7 +2,7 @@ import http.server
 import os
 
 PORT = 8000
-DIRECTORY = os.path.join(os.path.dirname(__file__), "dummy_website")
+DIRECTORY = os.path.join(os.path.dirname(__file__))
 
 class Handler(http.server.SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
