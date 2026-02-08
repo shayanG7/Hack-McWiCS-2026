6400d6309706cc7933f6a0517e877193212e8437
orianasion <orianasion777@gmail.com>
2026-02-07 23:54:52 -0500

    make it prettier

---
+++ dummy_website/assets/js/boxColourer.js
@@ -0,0 +1,21 @@
+function getRandomColor() {
+    const colors = [
+    '#F5E387', //yellow
+    '#CBED79', //lime
+    '#69A346', //green
+    '#46A394', //teal
+    '#4657A3', //blue
+    '#5946A3', //purple
+    '#A34671', //pink
+    ]
+
+    const randomColor = colors[Math.floor(Math.random() * 7)];
+    return randomColor;
+}
+
+function applyRandomColour() {
+    const elements = document.querySelectorAll('.thumb-color');
+    elements.forEach(element => {
+        element.style.backgroundColor = getRandomColor();
+    })
+}
